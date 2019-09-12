4da101a36c8ec2f03d66008aeaa2c1e8c6252f9e
oldbackside <thisjiayu@gmail.com>
2019-09-12 16:36:59 +0800

    uptext

---
+++ src/leetcode/textgit.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class textgit {
+    public static void sayhello() {
+    	System.out.println("hellogithub");
+    }
+    public static void main(String[] args) {
+		sayhello();
+	}
+}
+
